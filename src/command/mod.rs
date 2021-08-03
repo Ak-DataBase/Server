2faa12b3cc4b1a2a76f600d7e5841177a616f076
Brian3647 <victoris3647@gmail.com>
2021-08-03 16:02:22 +0000

    Removed useless file and moved the important code to an existing file

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,10 +1,13 @@
 pub mod args;
-pub mod run;
 
 pub use anyhow::Context;
 pub use structopt::StructOpt;
 
+use crate::server;
+
 pub fn main() -> anyhow::Result<()> {
-	run::run(args::Arguments::from_args())?;
+	let args = args::Arguments::from_args();
+	let port = args.port();
+	server::run(port)?;
 	Ok(())
 }
--- src/command/run.rs
+++
@@ -1,8 +0,0 @@
-use super::super::server;
-use super::args;
-
-pub fn run(args: args::Arguments) -> anyhow::Result<()> {
-	let port = args.port();
-	server::run(port)?;
-	Ok(())
-}
